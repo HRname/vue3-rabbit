455eaec9c8740da611d1dc0b3df22d4c703ee5d7
HRname <930235824@qq.com>
2024-04-02 17:57:17 +0800

    Hot区实现图片懒加载，定义全局懒加载指令

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import App from './App.vue'
 import router from './router'
 
 import '@/styles/common.scss'
+// 判断图片是否在窗口中的函数
+import { useIntersectionObserver } from '@vueuse/core'
 
 const app = createApp(App)
 
@@ -12,3 +14,18 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+// 定义全局指令实现图片懒加载
+
+app.directive('img-lazy',{
+    mounted(el,binding){
+        useIntersectionObserver(el,
+            ([{ isIntersecting }]) => {
+                console.log(isIntersecting)
+                if(isIntersecting){
+                    el.src = binding.value
+                }
+            }
+        )
+    }
+})
